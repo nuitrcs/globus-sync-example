2d28c144f7fe5c414b7e50c591a365c1f538b7ac
Matthew Rich <m-rich@northwestern.edu>
2018-02-13 11:02:16 -0600

    set shell variables correctly

--- run_globus_sync.sh
+++ run_globus_sync.sh
@@ -14,28 +14,29 @@
 # Create your app at https://auth.globus.org/v2/web/developers
 # be sure to check the "Native App" checkbox and set the redirect URL to
 # https://auth.globus.org/v2/web/auth-code
-GLOBUS_SYNC_CLIENT_ID = ad6382da-8ada-47e1-810b-d8010525688f
+GLOBUS_SYNC_CLIENT_ID=<PASTE YOUR CLIENT ID HERE>
 
 # This is the endpoint where data will be synced *from*.
 # The value below is the Northwestern Quest endpoint.
-SOURCE_ENDPOINT = d5990400-6d04-11e5-ba46-22000b92c6ec
+SOURCE_ENDPOINT=d5990400-6d04-11e5-ba46-22000b92c6ec
 
 # The path you want to sync on the source endpoint
-SOURCE_PATH = /home/mtr932
+# YOU MUST REPLACE THIS WITH THE PATH TO SYNC
+SOURCE_PATH=/projects/example
 
 # This is the endpoint where data will be synced *to*.
-DESTINATION_ENDPOINT = bdf3cd60-e128-11e7-802b-0a208f818180
+DESTINATION_ENDPOINT=<PASTE THE DESTINATION ENDPOINT ID HERE>
 
 # The path on the destination endpoint where data should be synced to.
-DESTINATION_PATH = /~/globus-sync-testing/
+DESTINATION_PATH=<DESTINATION PATH>
 
 # Can be one of 'exists', 'size', 'mtime', 'timestamp', or 'checksum'
 # See https://docs.globus.org/cli/reference/transfer/ for details
-SYNC_TYPE = checksum
+SYNC_TYPE=checksum
 
 # A friendly name for the transfer, will be visible in the Globus web interface
-TRANSFER_LABEL = "Globus Backup"
+TRANSFER_LABEL="Globus Backup"
 
 module load python/anaconda3.6
-python sync.py --synctype $SYNC_TYPE --transfer-label "$TRANSFER_LABEL" $SOURCE_ENDPOINT $SOURCE_PATH $DESTINATION_ENDPOINT $DESTINATION_PATH
+GLOBUS_SYNC_CLIENT_ID=$GLOBUS_SYNC_CLIENT_ID python sync.py --synctype $SYNC_TYPE --transfer-label "$TRANSFER_LABEL" $SOURCE_ENDPOINT $SOURCE_PATH $DESTINATION_ENDPOINT $DESTINATION_PATH
 
